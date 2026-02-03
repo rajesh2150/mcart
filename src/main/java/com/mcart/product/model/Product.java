4094957554209ef8900afc2b8f19ad30b0c19467
rajesh2150 <korlapati.rajesh@mphasis.com>
2026-02-03 16:57:22 +0530

    product model updated

--- src/main/java/com/mcart/product/model/Product.java
+++ src/main/java/com/mcart/product/model/Product.java
@@ -18,5 +18,63 @@ public class Product {
     int quantity;
 
 
+    public Product() {
+    }
 
+    public Product(int id, String productName, String description, double price, String category, int quantity) {
+        this.id = id;
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
