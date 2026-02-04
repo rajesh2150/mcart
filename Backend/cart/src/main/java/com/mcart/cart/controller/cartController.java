7a181527da2a3b9ed783522b06f16083cb6dca5f
rajesh2150 <korlapati.rajesh@mphasis.com>
2026-02-04 16:26:46 +0530

    fix: Controller Name is Updated

--- Backend/cart/src/main/java/com/mcart/cart/controller/cartController.java
+++ Backend/cart/src/main/java/com/mcart/cart/controller/cartController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/app/cart")
-public class cartController {
+public class CartController {
 
     @Autowired
     CartService cartservice;
